ebfe090089c4674ffad7d5409152dbe1a2ce35f5
Marlvin12 <marlvingore.edu@gmail.com>
2024-02-09 04:29:49 -0600

    functions

---
+++ Chorus.java
@@ -0,0 +1,17 @@
+public class Chorus {
+    public static void main(String[] args) {
+        // Chorus needs to be sung 5 times
+        singChorus();
+        singChorus();
+        singChorus();
+        singChorus();
+        singChorus();
+     
+    }
+    public static void singChorus(){
+         System.out.println("Don't blame it on the sunshine");
+        System.out.println("Don't blame it on the moonlight");
+        System.out.println("Don't blame it on good times");
+        System.out.println("Don't blame it on the boogie\n");
+    }
+}
