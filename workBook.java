05cf8e3bd3168246bf2cbdb9f832c4072a008f7a
Marlvin Goremusandu <122947486+Marlvin12@users.noreply.github.com>
2024-02-26 05:03:24 -0600

    Add files via upload

---
+++ workBook.java
@@ -0,0 +1,10 @@
+public class workBook {
+    public static void main(String[] args) {
+        for( int i=0;i<=10;i++){
+            for(int j=0;j<=10;j++){
+                System.out.println(j+". If Java was easy, they would call it Python!"); 
+            }
+        }   
+    }
+    
+}
